1f7a57fe506949464751099b19f67c50a6ebdda5
Dylanyessid <dylanyessid1904@gmail.com>
2026-05-03 17:55:04 -0500

    refactor: dont verify tls

--- src/config/db.ts
+++ src/config/db.ts
@@ -18,5 +18,8 @@ export const AppDataSource = new DataSource({
     password: envs.DB_PASSWORD,
     database: envs.DB_NAME,
     //logging: true,
+    ssl:{
+        rejectUnauthorized: false
+    },
     entities: [User, Client, Product, Warehouse, Port, LandShipment, SeaShipment],
 });
